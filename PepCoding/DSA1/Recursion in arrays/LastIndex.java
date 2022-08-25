2689daeb1ce61447d00494837f3dca5704c16b3f
Jitender Singh <73359865+sharpsailor@users.noreply.github.com>
2022-08-25 19:18:05 +0530

    Update LastIndex.java

--- PepCoding/DSA1/Recursion in arrays/LastIndex.java
+++ PepCoding/DSA1/Recursion in arrays/LastIndex.java
@@ -29,23 +29,37 @@ public class Main {
 /* This is 2nd approach which say to first iterate over the array and reach the last where we have already declared it -1 by assuming the base case,
 if this is -1 then now we will take a nested condition which says if found then store the last value at the index else we will store -1 after this at the end we will c
 come out of else condition which will return last value*/
- public static int lastIndex(arr,idx,x){
- if(idx==arr.length-1){
-   return -1;
- }
-   int lissa = lastIndex(arr,idx+1,x);
-   if(liisa==-1){
-    if(arr[idx]==x){
-      
-            return idx;
-    }else{
-     return -1;
+import java.io.*;
+import java.util.*;
+
+public class Main {
+
+  public static void main(String[] args) throws Exception {
+    // write your code here
+    Scanner sc = new Scanner(System.in);
+    int n = sc.nextInt();
+    int[]arr = new int[n];
+    for (int i = 0; i < n; i++) {
+      arr[i] = sc.nextInt();
     }
-     else{
-       return lissa;
-   }
- }
+    int x = sc.nextInt();
+    System.out.println(lastIndex(arr, 0, x));
+  }
 
+  public static int lastIndex(int[] arr,int idx,int x) {
+    if (idx == arr.length - 1) {
+      return -1;
+    }
+     int Liisa = lastIndex(arr,idx+1,x);
+    if(Liisa ==-1){
+      if (arr[idx] == x) {
+        return idx;
+      } else {
+        return -1;
+      }
+    }else {
+        return Liisa;
+      }
+    }
 
-
-
+  }
